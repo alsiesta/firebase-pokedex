68eb830cced17b3f728f7cf9b5f6bd7588d35834
alsiesta <as@daslizenzatelier.de>
2023-03-20 12:01:06 +0100

    installed firebase and environments

--- src/app/core/pokedex-firestore.service.ts
+++ src/app/core/pokedex-firestore.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { CollectionReference, DocumentData, collection} from '@firebase/firestore';
+
+import { Firestore } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokedexFirestoreService {
+  private pokemonCollection: CollectionReference<DocumentData>;
 
-  constructor() { }
+  constructor(private readonly firestore: Firestore) {
+    this.pokemonCollection = collection(this.firestore, 'pokemon');
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ng-pokedex-53866',
+    appId: '1:713396436554:web:8c98a08fea13e211e0b0b3',
+    storageBucket: 'ng-pokedex-53866.appspot.com',
+    apiKey: 'AIzaSyBmcH2UCyLKIWj6ofQBhrvskWosS2WRCWY',
+    authDomain: 'ng-pokedex-53866.firebaseapp.com',
+    messagingSenderId: '713396436554',
+  },};
