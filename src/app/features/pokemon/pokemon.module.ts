070edfb1f9f6da1e98a588ade93090de4db2e33d
alsiesta <as@daslizenzatelier.de>
2023-03-20 12:28:40 +0100

    added form component

--- src/app/features/pokemon/pokemon.module.ts
+++ src/app/features/pokemon/pokemon.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { PokemonRoutingModule } from './pokemon-routing.module';
 import { PokemonComponent } from './pokemon.component';
+import { FormComponent } from './components/form/form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    PokemonComponent
+    PokemonComponent,
+    FormComponent
   ],
-  imports: [
-    CommonModule,
-    PokemonRoutingModule
-  ]
+  imports: [CommonModule,PokemonRoutingModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatButtonModule,MatDialogModule]
 })
 export class PokemonModule { }
